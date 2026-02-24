f6ac26bf8f8b6130b93348cbbc3aa24c7ed98579
Iggysmallz <141804426+Iggysmallz@users.noreply.github.com>
2026-02-24 23:26:38 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!
+  "https://vchtdktlgxzmkiuuxsun.supabase.co",
+  "sb_publishable_m_5JuVQDUVpb5Bfm1Ut2RQ_-PVyNsEV"
 );
